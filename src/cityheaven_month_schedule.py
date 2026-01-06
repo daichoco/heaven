132c5412627de406484b29ec5c92c5e4b3ca4e0d
daichoco <daichoco08050214@gmail.com>
2026-01-07 00:00:32 +0900

    Disable headless mode for Chrome in cityheaven_month_schedule.py for better debugging visibility.

--- src/cityheaven_month_schedule.py
+++ src/cityheaven_month_schedule.py
@@ -13,7 +13,7 @@ import pandas as pd
 #%%
 chromedriver_autoinstaller.install()
 options = Options()
-options.add_argument("--headless=new")
+# options.add_argument("--headless=new")
 options.add_argument("--disable-gpu")
 options.add_argument("--window-size=1920,1080")
 options.add_argument("--disable-dev-shm-usage")
